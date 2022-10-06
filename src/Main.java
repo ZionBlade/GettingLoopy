c652c144098ae9296491b9180d3765a0217b2102
ZionBlade <wernerct@mail.uc.edu>
2022-10-06 13:37:43 -0400

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,59 @@
 public class Main {
     public static void main(String[] args)
     {
+
+        String s = "*";
+        // task 1
         for(int x=0; x<= 30 ; x++)
         {
             System.out.println(x);
+        }
+
+        for(int l=0; l < 3; l++)
+            System.out.println();
+
+
+
+
+
+
+
+
+        //task 5
+
+        for (int y=1; y <= 5; y++) //row
+        {
+            for (int x = 1 ; x <= y ; x++) //column
+            {
+                System.out.print(s);
+            }
+            System.out.println();
+        }
+
+        for(int l=0; l < 3; l++)
+            System.out.println();
+
+        //task 6
+        for (int y=1; y <= 5; y++) //row
+        {
+            for (int x = 5 ; x >= y ; x--) //column
+            {
+                System.out.print(s);
+            }
+            System.out.println();
+        }
+
+        for(int l=0; l < 3; l++)
+            System.out.println();
+
+        // task 7
+        for (int y=1; y <= 5; y++) //row
+        {
+            for (int x = 1 ; x <= 5 ; x++) //column
+            {
+                System.out.print(s);
+            }
+            System.out.println();
         }
     }
 }
